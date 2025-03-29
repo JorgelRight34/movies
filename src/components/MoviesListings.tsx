5931f20a1b8c9cb5ad89a60cf7f06ba06557ecac
JorgelRight34 <JorgelRight34RD30012003@gmail.com>
2025-03-28 22:32:06 -0400

    Before starting writing tests

--- src/components/MoviesListings.tsx
+++ src/components/MoviesListings.tsx
@@ -1,8 +1,12 @@
 import { useMemo } from "react";
-import useMovies from "../hooks/useMovies";
+
 import MovieCard from "./MovieCard";
 import { Movie } from "../models/movie";
 import { NavLink } from "react-router";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface MoviesListingsProps {
   movies: Movie[];
@@ -23,17 +27,28 @@ const MoviesListings = ({
     [movies]
   );
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+  };
+
   return (
     <div className={`movies-listings gap-${gap}`}>
-      {orderedMovies.map((movie) => (
-        <NavLink
-          key={movie.id}
-          className={`text-decoration-none col-lg-${width}`}
-          to={`/movies/${movie.id}`}
-        >
-          <MovieCard key={movie.id} movie={movie} />
-        </NavLink>
-      ))}
+      {orderedMovies.length}
+      <>
+        {orderedMovies.map((movie) => (
+          <NavLink
+            key={movie.id}
+            className={`text-decoration-none col-lg-${width}`}
+            to={`/movies/${movie.id}`}
+          >
+            <MovieCard key={movie.id} movie={movie} />
+          </NavLink>
+        ))}
+      </>
     </div>
   );
 };
